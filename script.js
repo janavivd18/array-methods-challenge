2bcb9982d48d4504939531cdf444831187b1d481
janavivd18@gmail.com <janavivd@gmail.com>
2026-07-27 08:33:21 +0530

    first repo

---
+++ script.js
@@ -0,0 +1,23 @@
+let x=Math.floor(Math.random()*100)+1;
+let y=Math.floor(Math.random()*500)+1;
+
+let sum=x+y;
+let addition=`${x}+${y}=${sum}`;
+console.log(addition);
+
+let difference=x-y;
+let subtract=`${x}-${y}=${difference}`;
+console.log(subtract);
+
+let product=x*y;
+let multiply=`${x}*${y}=${product}`;
+console.log(multiply);
+
+let quotient=x/y;
+let quotientoutput=`${x}/${y}=${quotient}`;
+console.log(quotientoutput);
+
+let rm=x%y;
+let rmoutput=`${x}%${y}=${rm}`;
+console.log(rmoutput);
+
